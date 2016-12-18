fbd6e8d44953aa37de6dcc8249da2731357af377
meluk <kmelisu@hotmail.com>
2016-12-18 15:11:57 -0600

    mejora reportes

--- app/controllers/report.js
+++ app/controllers/report.js
@@ -3,32 +3,64 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	customers = mongoose.model('customers');
 
-var promedio,
-    time= new Date();
-	date = time.toISOString().slice(0,10);
+var time = new Date(),
+	date = time.toISOString().slice(0,10),
+	totaArray;
 
-function Buscar(array){
-	for (var i = array.length - 1; i >= 0; i--) {
-		console.log(array[i]._id);
-		console.log(date);
-		if (array[i]._id === date) {
-			console.log('solobuscar  '+array[i]._id);
-			customers.find({ 'fecha': array[i]._id, 'tiempoVentanilla' : array[i].maxQuantity },function(err, resVentanillas)
-			{
-				if (err) return next(err);
-			    console.log(customers);
-			    promedio = result;
-				
-			});
-		};
-	};
+// Funcion que devuelve el tiempo transcurrido en dias, horas, minutos y segundos
+// Tiene que recibir los milisegundos
+function returnTime(time)
+{
+    // obtenemos los segundos
+    var time = time / 1000;
+ 
+    var result="";
+    if(time<60)
+    {
+        // unicamente mostraremos los segundos
+        result=Math.round(time)+" segundos";
+    }else{
+        // cogemos la parte entera de los segundos
+        var seconds = Math.round(time % 60);
+ 
+        // restamos los segundos que hemos cogido
+        time = Math.floor(time / 60);
+ 
+        // cogemos los minutos
+        var minutes = Math.round(time % 60);
+ 
+        // restamos los minutos que hemos cogido
+        time = Math.floor(time / 60);
+ 
+        // cogemos las horas
+        var hours = Math.round(time % 24);
+ 
+        // restamos las horas que hemos cogido
+        time = Math.floor(time / 24);
+ 
+        // el resto, son dias
+        var days = time;
+ 
+        if(days>0)
+        {
+            result=days+" dias, "+hours+" horas, "+minutes+" minutos y "+seconds+" segundos";
+        }else if(hours>0){
+            result=hours+" horas, "+minutes+" minutos y "+seconds+" segundos";
+        }else{
+            result=minutes+" minutos y "+seconds+" segundos";
+        }
+    }
+    return result;
 }
+
+//----------------------- app ------------
 
 module.exports = function(app)
 {
 	app.use('/', router);
 };
 
+//Reporte Timepo Aproximado que tarda cada ventanilla
 router.get('/report', function(req, res)
 {
 	customers.aggregate
@@ -38,25 +70,37 @@ router.get('/report', function(req, res)
 	       $group:
 	         {
 	           _id: "$tipoVentanilla",
-	           avgQuantity: { $avg: "$tiempoVentanilla" }
+	           Quantity: { $avg: "$tiempoVentanilla" }
 	         }
 	     }
 	   ],
 	   function(err,result) {
 	   	if (err) return next(err);
-	   	console.log(result[1]);
-	   	promedio = result;
+
+	   	var ReportTimeWindow = new Array();
+
+	   	for (var i = result.length - 1; i >= 0; i--) {
+	   		var newTime = returnTime(result[i].Quantity);
+	   		console.log(newTime);
+	   		var array= {
+	   			"_id" :  result[i]._id,
+	   			"Quantity" : newTime
+	   		};
+	   		ReportTimeWindow.push(array)
+	   		
+	   	};
+
+	   	res.render('report', {	
+			numDato: 'Timepo',
+			tituloDato: 'Ventanilla',
+			titulo: 'Medida de Tiempo por Ventanilla',
+			customers: ReportTimeWindow
+		});
        }
 	);
-    //promedio = customers;
-    
-   // console.log(customers);
-    res.render('report', {	//Hay un view que se llama Tienda
-		titulo: 'lista de customers',
-		customers: promedio
-	});
 });
 
+//Reporte cliente que dura mas por día
 router.get('/report2', function(req, res)
 {
 	customers.aggregate
@@ -66,23 +110,132 @@ router.get('/report2', function(req, res)
 	       $group:
 	         {
 	           _id: "$fecha",
-	           maxQuantity: { $max: "$tiempoVentanilla" }
+	           Quantity: { $max: "$tiempoVentanilla" }
 	         }
 	     }
 	   ],
 	   function(err,result) {
 	   	if (err) return next(err);
 	   	console.log(result);
-	   	promedio = result;
-	   	Buscar(promedio);
+
+	   	var ReportTimeWindow = new Array();
+
+        //Camobio los milisegundos a segundor/minutos/horas
+	   	for (var i = result.length - 1; i >= 0; i--) {
+	   		var newTime = returnTime(result[i].Quantity);
+
+		   		var array= {
+		   			"_id" :  result[i]._id,
+		   			"Quantity" : newTime
+		   		};
+		   		ReportTimeWindow.push(array)
+	   		
+	   	};
+
+	   	res.render('report', {	//vista
+			customers: ReportTimeWindow,
+			titulo: 'Cliente tardo más',
+			numDato: 'Tiempo cliente',
+			tituloDato: 'Fecha',
+		});
        }
 	);
-    //promedio = customers;
-    
-   // console.log(customers);
-    	
-	res.render('report', {	//Hay un view que se llama Tienda
-		customers: promedio
-	});
 });
 
+//Reporte cliente que dura mas por día
+router.get('/report3', function(req, res)
+{
+	customers.aggregate
+	(
+	   [
+	     {
+	       $group:
+	         {
+	           _id: "$fecha",
+	           Quantity: { $min: "$tiempoVentanilla" }
+	         }
+	     }
+	   ],
+	   function(err,result) {
+	   	if (err) return next(err);
+	   	console.log(result);
+
+	   	var ReportTimeWindow = new Array();
+
+        //Camobio los milisegundos a segundor/minutos/horas
+	   	for (var i = result.length - 1; i >= 0; i--) {
+	   		var newTime = returnTime(result[i].Quantity);
+
+		   		var array= {
+		   			"_id" :  result[i]._id,
+		   			"Quantity" : newTime
+		   		};
+		   		ReportTimeWindow.push(array)
+	   		
+	   	};
+
+	   	res.render('report', {	//vista
+			customers: ReportTimeWindow,
+			titulo: 'Cliente tardo menos',
+			numDato: 'Tiempo cliente',
+			tituloDato: 'Fecha',
+		});
+       }
+	);
+});
+
+//Reporte Cantidada de Clientes
+router.get('/reportCountCoustemers', function(req, res)
+{
+	customers.aggregate
+	(
+	   [
+	     {
+	       $group:
+	         {
+	           _id: "$fecha",
+	           Quantity: { $sum: 1 }
+	         }
+	     }
+	   ],
+	   function(err,result) {
+	   	if (err) return next(err);
+	   	res.render('report', {	
+			numDato: 'Total Clientes',
+			tituloDato: 'Fecha',
+			titulo: 'Total de Clientes por Fecha',
+			customers: result
+		});
+       }
+	);
+	       
+});
+
+//Reporte Cantidada de Clientes por Ventanilla
+router.get('/reportCountWindows', function(req, res)
+{
+	customers.aggregate
+	(
+	   [
+	     {
+	       $group:
+	         {
+	           _id: "$tipoVentanilla",
+	           Quantity: { $sum: 1 }
+	         }
+	     }
+	   ],
+	   function(err,result) {
+	   	if (err) return next(err);
+
+	   	console.log(result);
+	   	res.render('report', {	
+			numDato: 'total Clientes',
+			tituloDato: 'Ventanilla',
+			titulo: 'Total de Clientes por ventanilla',
+			customers: result
+		});
+       }
+	);
+	       
+});
